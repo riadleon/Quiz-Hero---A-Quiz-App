3b8a2ccd11f000309a530568f0c02f5ae8b8376b
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-10-12 00:31:28 +0600

    few more tasks left

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     return (
         <section>
             <div className="bg-gray-100">
-                <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-900">
+                <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-40 md:py-32 md:px-10 lg:px-32 text-gray-900">
                     <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-800">
                         Welcome To Quiz Hero Application
                     </h1>
